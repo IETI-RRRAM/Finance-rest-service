73d139cb764076693cf17d455a799209942cf4a6
Juan Felipe Aguas Pulido <aguasfelipe02@gmail.com>
2023-04-12 08:28:23 -0500

    Added service impl

--- src/main/java/edu/eci/agronomo/finance/service/FinanceServiceImpl.java
+++ src/main/java/edu/eci/agronomo/finance/service/FinanceServiceImpl.java
@@ -2,6 +2,8 @@ package edu.eci.agronomo.finance.service;
 
 import edu.eci.agronomo.finance.model.finance.Finance;
 import edu.eci.agronomo.finance.model.finance.FinanceDto;
+import edu.eci.agronomo.finance.repository.FinanceRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,29 +12,31 @@ import java.util.Optional;
 @Service
 public class FinanceServiceImpl implements FinanceService {
 
+    @Autowired
+    FinanceRepositoryImpl financeRepository;
 
     @Override
     public ArrayList<Finance> findAll() {
-        return null;
+        return financeRepository.findAll();
     }
 
     @Override
     public Optional<Finance> findById(String id) {
-        return Optional.empty();
+        return financeRepository.findById(id);
     }
 
     @Override
     public Finance save(FinanceDto financeDto) {
-        return null;
+        return financeRepository.save(financeDto);
     }
 
     @Override
     public void delete(String id) {
-
+        financeRepository.delete(id);
     }
 
     @Override
     public Finance update(String id, FinanceDto financeDto) {
-        return null;
+        return financeRepository.update(id, financeDto);
     }
 }
